9ed12c2dd2740a2649a1b91ff9c2079d0a8e324d
huangxf14 <464271301@qq.com>
2016-02-26 14:10:26 +0800

    add shield for longAttack

--- ts17core/gamemain.py
+++ ts17core/gamemain.py
@@ -430,7 +430,8 @@ class GameMain:
     #判断远程攻击是否可以命中
     def longAttackbullet(self,bulletId:int):
         if (self.dis(self._scene.getObject(bulletId).center,self._scene.getObject(self._objects[bulletId].enemy).center)<self._objects[bulletId].speed):
-            self.healthDown(self._objects[bulletId].enemy, self._objects[bulletId].damage)
+            if self._players[self._objects[bulletId].enemy].shieldTime==0 and self._players[self._objects[bulletId].enemy].shieldLevel<5:
+                self.healthDown(self._objects[bulletId].enemy, self._objects[bulletId].damage)
             if (self._objects[bulletId].stop==True):
                 self._players[self._objects[bulletId].enemy].stoptime=30
             self._scene.delete(bulletId)
